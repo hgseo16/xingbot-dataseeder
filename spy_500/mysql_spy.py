6f4248be73167b612251bd49e97e34d41796ea48
hgseo16 <seohyounggyoo@gmail.com>
2022-05-29 20:11:57 +0900

    Query insert fixed

--- spy_500/mysql_spy.py
+++ spy_500/mysql_spy.py
@@ -11,8 +11,7 @@ def mysql_spy(date_list, time_list, open_list, high_list, low_list, price_list, 
 
     sql_set_table_daily_data = '''
     CREATE TABLE SPY_500_daily 
-    (ID INT NOT NULL PRIMARY KEY,
-    날짜 VARCHAR(30) NULL,
+    (날짜 VARCHAR(30) NOT NULL PRIMARY KEY,
     시간 VARCHAR(30) NULL,
     시가 FLOAT NULL,
     고가 FLOAT NULL,
@@ -26,14 +25,6 @@ def mysql_spy(date_list, time_list, open_list, high_list, low_list, price_list, 
     한국시간 VARCHAR(30) NULL)
     '''
 
-    sql_insert_daily_data = '''
-    INSERT INTO SPY_500_DAILY
-    (ID, 날짜, 시간, 시가, 고가, 저가, 가격, 
-    전일대비구분, 전일대비, 등락률, 누적거래량, 한국일자, 한국시간)
-    VALUE
-    (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    '''
-
     conn = pymysql.connect(host='127.0.0.1', user='root', password=AUTHENTICATION_PASSWORD, charset='utf8')
 
     try:
@@ -43,21 +34,21 @@ def mysql_spy(date_list, time_list, open_list, high_list, low_list, price_list, 
         conn.select_db('SPY_500')
         curs.execute(sql_set_table_daily_data)
         for i in range(len(date_list)):
-            test = '''
+            sql_insert_daily_data = '''
             INSERT INTO SPY_500_DAILY
-            (ID, 날짜, 시간, 시가, 고가, 저가, 가격, 
+            (날짜, 시간, 시가, 고가, 저가, 가격, 
             전일대비구분, 전일대비, 등락률, 누적거래량, 한국일자, 한국시간)
             VALUE
-            ({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})
-            '''.format(str(i), str(date_list[i]), str(time_list[i]),str(open_list[i]),str(high_list[i]), str(low_list[i]), str(price_list[i]),str(sign_list[i]),str(change_list[i]),str(uprate_list[i]),str(volume_list[i]),str(kodate_list[i]), str(kotime_list[i]))
+            ({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})
+            '''.format(str(date_list[i]), str(time_list[i]),str(open_list[i]),str(high_list[i]), str(low_list[i]), str(price_list[i]),str(sign_list[i]),str(change_list[i]),str(uprate_list[i]),str(volume_list[i]),str(kodate_list[i]), str(kotime_list[i]))
             # print(test)
-            curs.execute(test)
+            curs.execute(sql_insert_daily_data)
         conn.commit()
-        curs.execute('SELECT * FROM SPY_500_DAILY')
 
-        sql_row = curs.fetchall()
-        for i in sql_row:
-            print(i)
+        # curs.execute('SELECT * FROM SPY_500_DAILY')
+        # sql_row = curs.fetchall()
+        # for i in sql_row:
+        #     print(i)
 
     finally:
         conn.close()
--- spy_500/mysql_spy_backup.py
+++ spy_500/mysql_spy_backup.py
@@ -3,45 +3,61 @@ import xlrd
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+def mysql_spy(date_list, time_list, open_list, high_list, low_list, price_list, sign_list, change_list, uprate_list, volume_list, kodate_list, kotime_list):
 
-AUTHENTICATION_PASSWORD = os.environ.get('AUTHENTICATION_PASSWORD')
+    load_dotenv()
 
-sql_set_table_daily_data = '''
-CREATE TABLE SPY_500_daily
-                 (ID INT NOT NULL PRIMARY KEY,
-                 날짜 VARCHAR(30) NULL,
-                 시간 VARCHAR(30) NULL,
-                 시가 FLOAT NULL,
-                 고가 FLOAT NULL,
-                 저가 FLOAT NULL,
-                 가격 FLOAT NULL,
-                 전일대비구분 FLOAT NULL,
-                 전일대비 FLOAT NULL,
-                 등락률 FLOAT NULL,
-                 누적거래량 FLOAT NULL,
-                 한국일자 VARCHAR(30) NULL,
-                 한국시간 VARCHAR(30) NULL)
-'''
+    AUTHENTICATION_PASSWORD = os.environ.get('AUTHENTICATION_PASSWORD')
 
-sql_insert_daily_data = '''
-INSERT INTO SPY_500_daily
-(ID, 날짜, 시간, 시가, 고가, 저가, 가격, 
-전일대비구분, 전일대비, 등락률, 누적거래량, 한국일자, 한국시간)
- VALUE
- (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-'''
+    sql_set_table_daily_data = '''
+    CREATE TABLE SPY_500_daily 
+    (ID INT NOT NULL PRIMARY KEY,
+    날짜 VARCHAR(30) NULL,
+    시간 VARCHAR(30) NULL,
+    시가 FLOAT NULL,
+    고가 FLOAT NULL,
+    저가 FLOAT NULL,
+    가격 FLOAT NULL,
+    전일대비구분 FLOAT NULL,
+    전일대비 FLOAT NULL,
+    등락률 FLOAT NULL,
+    누적거래량 FLOAT NULL,
+    한국일자 VARCHAR(30) NULL,
+    한국시간 VARCHAR(30) NULL)
+    '''
 
-conn = pymysql.connect(host='127.0.0.1', user='root', password=AUTHENTICATION_PASSWORD, charset='utf8')
-# workboo = xlrd.open_workbook('삼성전자.xls', encoding_override='cp949')
+    sql_insert_daily_data = '''
+    INSERT INTO SPY_500_DAILY
+    (ID, 날짜, 시간, 시가, 고가, 저가, 가격, 
+    전일대비구분, 전일대비, 등락률, 누적거래량, 한국일자, 한국시간)
+    VALUE
+    (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    '''
 
-try:
-    curs = conn.cursor()
-    curs.execute('DROP DATABASE IF EXISTS SPY_500')
-    curs.execute('CREATE DATABASE SPY_500')
-    conn.select_db('SPY_500')
-    curs.execute(sql_set_table_daily_data)
-    curs.execute(sql_insert_daily_data)
+    conn = pymysql.connect(host='127.0.0.1', user='root', password=AUTHENTICATION_PASSWORD, charset='utf8')
 
-finally:
-    conn.close()
+    try:
+        curs = conn.cursor()
+        curs.execute('DROP DATABASE IF EXISTS SPY_500')
+        curs.execute('CREATE DATABASE SPY_500')
+        conn.select_db('SPY_500')
+        curs.execute(sql_set_table_daily_data)
+        for i in range(len(date_list)):
+            test = '''
+            INSERT INTO SPY_500_DAILY
+            (ID, 날짜, 시간, 시가, 고가, 저가, 가격, 
+            전일대비구분, 전일대비, 등락률, 누적거래량, 한국일자, 한국시간)
+            VALUE
+            ({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})
+            '''.format(str(i), str(date_list[i]), str(time_list[i]),str(open_list[i]),str(high_list[i]), str(low_list[i]), str(price_list[i]),str(sign_list[i]),str(change_list[i]),str(uprate_list[i]),str(volume_list[i]),str(kodate_list[i]), str(kotime_list[i]))
+            # print(test)
+            curs.execute(test)
+        conn.commit()
+        curs.execute('SELECT * FROM SPY_500_DAILY')
+
+        sql_row = curs.fetchall()
+        for i in sql_row:
+            print(i)
+
+    finally:
+        conn.close()
