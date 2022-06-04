6a38ebb7b15221e0fa500e8850e9128ef7d392af
hgseo16 <seohyounggyoo@gmail.com>
2022-06-04 18:52:00 +0900

    spy_500_backtester.py buy condition updated

--- spy_500/spy_500_backtester.py
+++ spy_500/spy_500_backtester.py
@@ -1,6 +1,5 @@
 import time as timefnc
 import datetime
-import pythoncom as pcom
 import pymysql
 import os
 from dotenv import load_dotenv
@@ -10,10 +9,13 @@ class Backtest_Engine:
     def __init__(self):
         print('Backtest_Engine Running...')
         self.initial_capital = 10000
+        self.available_capital = 10000
+        self.end_capital = self.initial_capital
         self.buy_commission = 0.0
         self.sell_commission = 0.0
-        self.start_date = None
-        self.end_date = datetime.datetime.now()
+        # Unused for now
+        # self.start_date = None
+        # self.end_date = datetime.datetime.now()
 
         load_dotenv()
 
@@ -32,22 +34,52 @@ class Backtest_Engine:
 
         self.Strategy()
 
-    def get_SMA(self, SMA_list):
-        size = len(SMA_list)
-        return sum(SMA_list) / size
+
+    def get_SMA(self, SMA_list, size):
+        i = len(SMA_list)
+        window = SMA_list[i-size:]
+
+        return sum(window) / size
 
 
     def Strategy(self):
+        SMA_3_list = []
         SMA_5_list = []
         SMA_10_list = []
         SMA_20_list = []
+        SMA_3 = None
         SMA_5 = None
         SMA_10 = None
         SMA_20 = None
 
         for row in self.rs:
-            SMA_5_list.append()
-            SMA_10_list.append()
-            SMA_20_list.append()
-            print(row)
-        print(len(self.rs))
+            curr_price = row[5]
+            # row(5) is the closing price (가격)
+            SMA_3_list.append(row[5])
+            SMA_5_list.append(row[5])
+            SMA_10_list.append(row[5])
+            SMA_20_list.append(row[5])
+
+            # Maintains list size according to SMA
+            if len(SMA_3_list) > 3:
+                del SMA_3_list[0]
+            if len(SMA_5_list) > 5:
+                del SMA_5_list[0]
+            if len(SMA_10_list) > 10:
+                del SMA_10_list[0]
+            if len(SMA_20_list) > 20:
+                del SMA_20_list[0]
+
+            # Calculate SMAs
+            if len(SMA_3_list) == 3:
+                SMA_3 = self.get_SMA(SMA_20_list, 3)
+            if len(SMA_5_list) == 5:
+                SMA_5 = self.get_SMA(SMA_20_list, 5)
+            if len(SMA_10_list) == 10:
+                SMA_10 = self.get_SMA(SMA_20_list, 10)
+            if len(SMA_20_list) == 20:
+                SMA_20 = self.get_SMA(SMA_20_list, 20)
+
+            # Buy Condition
+            if (curr_price > SMA_3) or (curr_price > SMA_5) or (curr_price > SMA_10):
+                self.initial_capital
