a08234d8e0e1d298c71408f186555da75b37a65c
hgseo16 <seohyounggyoo@gmail.com>
2022-06-17 22:00:54 +0900

    merge conflict

--- kospi_kosdaq/kospi_kosdaq_daily.py
+++ kospi_kosdaq/kospi_kosdaq_daily.py
@@ -20,37 +20,26 @@ class EC_t1903:
     # store shcode
     shcode = ''
 
-    # daily, weekly, monthly, 1min, 3min, 5min, 60min
     time_frame = ''
 
-    # Checks whether data is seeded
-    seeded = ''
-
-    # KOSPI / KOSDAQ / ETF
-    market = ''
+    counter = 1
 
     def OnReceiveData(self, code):
 
-
         if code == "t1903":
             occurs_count = self.GetBlockCount("t1903OutBlock1")
-            print(occurs_count)
             cts_date = self.GetFieldData("t1903OutBlock", "date", 0)
             print('cts_date: {}'.format(cts_date))
             # 종목명
             hname = self.GetFieldData("t1903OutBlock", "hname", 0)
             hname = hname.replace(" ", "_")
-            print(hname)
             # 업종지수명
             # upname = self.GetFieldData("t1903OutBlock", "upname", 0)
 
             if self.first_loop == True:
 
-                initialize_db(self.market, self.time_frame)
+                initialize_db(hname, self.time_frame)
                 self.first_loop = False
-
-                # Check whether table exists and create table if it doesn't
-                check_table(hname, self.market, self.time_frame)
 
             print('first loop done')
 
@@ -70,21 +59,32 @@ class EC_t1903:
                 jichange = self.GetFieldData("t1903OutBlock1", "jichange", inverse_idx)
                 jirate = self.GetFieldData("t1903OutBlock1", "jirate", inverse_idx)
 
-                # print("date: {}".format(type(date)))
-                # print("price: {}".format(type(price)))
-                # print("sign: {}".format(type(sign)))
-                # print("change: {}".format(type(change)))
-                # print("volume: {}".format(type(volume)))
-                # print("navdiff: {}".format(type(navdiff)))
-                # print("nav: {}".format(type(nav)))
-                # print("navchange: {}".format(type(navchange)))
-                # print("crate: {}".format(type(crate)))
-                # print("grate: {}".format(type(grate)))
-                # print("jisu: {}".format(type(jisu)))
-                # print("jichange: {}".format(type(jichange)))
-                # print("jirate: {}".format(type(jirate)))
+            # print("date: {}".format(type(date)))
+            # print("price: {}".format(type(price)))
+            # print("sign: {}".format(type(sign)))
+            # print("change: {}".format(type(change)))
+            # print("volume: {}".format(type(volume)))
+            # print("navdiff: {}".format(type(navdiff)))
+            # print("nav: {}".format(type(nav)))
+            # print("navchange: {}".format(type(navchange)))
+            # print("crate: {}".format(type(crate)))
+            # print("grate: {}".format(type(grate)))
+            # print("jisu: {}".format(type(jisu)))
+            # print("jichange: {}".format(type(jichange)))
+            # print("jirate: {}".format(type(jirate)))
 
-                mysql_etf(hname, self.market, self.time_frame, date, price, sign, change, volume, navdiff, nav, navchange, crate, grate, jisu, jichange, jirate)
+                # TEST
+                # if self.counter > 2:
+                #     check_if_exist = '''
+                #    SELECT * FROM daily
+                #    WHERE 일자 = 20220418
+                #    '''
+                #     EC_t1903.curs.execute(check_if_exist)
+                #     print(EC_t1903.curs.fetchall())
+                #
+
+
+                mysql_etf(hname, self.time_frame, date, price, sign, change, volume, navdiff, nav, navchange, crate, grate, jisu, jichange, jirate)
 
             if cts_date != "":
                 t1903_request(shcode=EC_t1903.shcode, date=cts_date, time_frame=self.time_frame, occurs=self.IsNext)
@@ -95,56 +95,56 @@ class EC_t1903:
 
 def t1903_request(shcode=None, date=None, time_frame='', seeded=True, market='', occurs=True):
 
+    # Temp: so that it destroy data
+    if seeded == False:
+        # Should do what it's currently doing RN
+        return
+
     timefnc.sleep(3.1)
 
-    # Pass type of "market"
-    EC_t1903.market = market
-
     # Pass time_frame (daily, 1min, 3min, etc) to method
-    EC_t1903.time_frame = time_frame
+    if EC_t1903.first_loop == True:
+        EC_t1903.time_frame = time_frame
 
-    # Pass whether it's been "seeded"
-    EC_t1903.seeded = seeded
-
-    EC_t1903.shcode = shcode
+    EC_t1903.seeded=seeded
 
     EC_t1903.t1903_e.SetFieldData("t1903InBlock", "shcode", 0, shcode)
     EC_t1903.t1903_e.SetFieldData("t1903InBlock", "date", 0, date)
     EC_t1903.t1903_e.Request(occurs)
 
     EC_t1903.tr_success = False
+    EC_t1903.shcode = shcode
 
     while EC_t1903.tr_success == False:
         pcom.PumpWaitingMessages()
 
 
+def mysql_etf(hname, time_frame, date, price, sign, change, volume, navdiff, nav, navchange, crate, grate, jisu, jichange, jirate):
 
-def mysql_etf(hname, market, time_frame, date, price, sign, change, volume, navdiff, nav, navchange, crate, grate, jisu, jichange, jirate):
+        # curs.execute('DROP DATABASE IF EXISTS SPY_500')
+        # curs.execute('CREATE DATABASE SPY_500')
+        EC_t1903.conn.select_db('{}'.format(hname))
+        # curs.execute(sql_set_table_daily_data)
 
-    EC_t1903.conn.select_db('{}_{}'.format(market, time_frame))
+        print(hname)
 
-    print(hname)
+        sql_insert_daily_data = '''
+        INSERT INTO {}
+        (일자, 현재가, 전일대비구분, 전일대비, 누적거래량, NAV대비, 
+        NAV, NAV전일대비, 추적오차, 괴리, 지수, 지수전일대비, 지수전일대비율)
+        VALUE
+        ({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})
+        '''.format(time_frame, str(date), str(price), str(sign), str(change), str(volume), str(navdiff), str(nav), str(navchange),
+                   str(crate), str(grate), str(jisu), str(jichange), str(jirate))
 
-    sql_insert_daily_data = '''
-    INSERT INTO {}
-    (일자, 현재가, 전일대비구분, 전일대비, 누적거래량, NAV대비, 
-    NAV, NAV전일대비, 추적오차, 괴리, 지수, 지수전일대비, 지수전일대비율)
-    VALUE
-    ({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})
-    '''.format(hname, str(date), str(price),
-               str(sign), str(change), str(volume),
-               str(navdiff), str(nav), str(navchange),
-               str(crate), str(grate), str(jisu),
-               str(jichange), str(jirate))
+        print(sql_insert_daily_data)
+        EC_t1903.curs.execute(sql_insert_daily_data)
+        EC_t1903.conn.commit()
 
-    print(sql_insert_daily_data)
-    EC_t1903.curs.execute(sql_insert_daily_data)
-    EC_t1903.conn.commit()
-
-    EC_t1903.counter+=1
+        EC_t1903.counter+=1
 
 
-def initialize_db(market, time_frame):
+def initialize_db(hname, time_frame):
 
     load_dotenv()
 
@@ -158,14 +158,14 @@ def initialize_db(market, time_frame):
     # curs.execute('DROP DATABASE IF EXISTS KODEX_LEVERAGE')
 
     # to check if db exists
-    EC_t1903.curs.execute("SHOW DATABASES LIKE '{}_{}'".format(market, time_frame))
+    EC_t1903.curs.execute("SHOW DATABASES LIKE '{}'".format(hname))
     db_check = EC_t1903.curs.fetchall()
-    print(db_check)
+
 
     if db_check == (): # db doesn't exist
         print('db doesn\'t exist')
-        EC_t1903.curs.execute("CREATE DATABASE `{}_{}`".format(market, time_frame))
-        EC_t1903.conn.select_db("{}_{}".format(market, time_frame))
+        EC_t1903.curs.execute("CREATE DATABASE `{}`".format(hname))
+        EC_t1903.conn.select_db("{}".format(hname))
         # sql_set_table_daily_data = '''
         # CREATE TABLE `{}`
         # (일자 VARCHAR(30) NOT NULL PRIMARY KEY,
@@ -183,61 +183,52 @@ def initialize_db(market, time_frame):
         # 지수전일대비율 FLOAT NULL)
         # '''.format(time_frame)
 
-        # EC_t1903.conn.select_db("{}_{}".format(market, time_frame))
-        EC_t1903.conn.commit()
-
-    else: # db exist
-        print('db exist')
-        EC_t1903.curs.execute("DROP DATABASE IF EXISTS {}_{}".format(market, time_frame))
-        EC_t1903.curs.execute("CREATE DATABASE {}_{}".format(market, time_frame))
-        print("CREATE DATABASE {}_{}".format(market, time_frame))
-
-        EC_t1903.conn.commit()
-
-def check_table(hname, market, time_frame):
-
-    # Select DB
-    EC_t1903.conn.select_db("{}_{}".format(market, time_frame))
-
-    show_tables = '''
-    SHOW TABLES LIKE '{}'
-    '''.format(hname)
-
-    print(show_tables)
-    EC_t1903.curs.execute(show_tables)
-
-    table = EC_t1903.curs.fetchall()
-    print(table)
-
-    if table == (): # Table doesn't exist
-        # Create Table
-
-        # Query in string for creating hname table
         sql_set_table_daily_data = '''
-           CREATE TABLE `{}`
-           (일자 VARCHAR(30) NOT NULL PRIMARY KEY,
-           현재가 FLOAT NULL,
-           전일대비구분 FLOAT NULL,
-           전일대비 FLOAT NULL,
-           누적거래량 FLOAT NULL,
-           NAV대비 FLOAT NULL,
-           NAV FLOAT NULL,
-           NAV전일대비 FLOAT NULL,
-           추적오차 FLOAT NULL,
-           괴리 FLOAT NULL,
-           지수 FLOAT NULL,
-           지수전일대비 FLOAT NULL,
-           지수전일대비율 FLOAT NULL)
-           '''.format(hname)
-        print(sql_set_table_daily_data)
+        CREATE TABLE `{}`
+        (일자 VARCHAR(30) NOT NULL PRIMARY KEY,
+        현재가 FLOAT NULL,
+        전일대비구분 FLOAT NULL,
+        전일대비 FLOAT NULL,
+        누적거래량 FLOAT NULL,
+        NAV대비 FLOAT NULL,
+        NAV FLOAT NULL,
+        NAV전일대비 FLOAT NULL,
+        추적오차 FLOAT NULL,
+        괴리 FLOAT NULL,
+        지수 FLOAT NULL,
+        지수전일대비 FLOAT NULL,
+        지수전일대비율 FLOAT NULL)
+        '''.format(time_frame)
+        EC_t1903.curs.execute(sql_set_table_daily_data)
+        EC_t1903.conn.select_db("{}".format(hname))
+        EC_t1903.conn.commit()
+    else: # db exist
+        # TEST
+        EC_t1903.curs.execute('DROP DATABASE IF EXISTS {}'.format(hname))
+        print(hname)
+        EC_t1903.curs.execute('CREATE DATABASE {}'.format(hname))
+        print('CREATE DATABASE {}'.format(hname))
+        #
+
+        EC_t1903.conn.select_db("{}".format(hname))
+
+        # TEST
+        sql_set_table_daily_data = '''
+        CREATE TABLE `{}`
+        (일자 VARCHAR(30) NOT NULL PRIMARY KEY,
+        현재가 FLOAT NULL,
+        전일대비구분 FLOAT NULL,
+        전일대비 FLOAT NULL,
+        누적거래량 FLOAT NULL,
+        NAV대비 FLOAT NULL,
+        NAV FLOAT NULL,
+        NAV전일대비 FLOAT NULL,
+        추적오차 FLOAT NULL,
+        괴리 FLOAT NULL,
+        지수 FLOAT NULL,
+        지수전일대비 FLOAT NULL,
+        지수전일대비율 FLOAT NULL)
+        '''.format(time_frame)
         EC_t1903.curs.execute(sql_set_table_daily_data)
         EC_t1903.conn.commit()
-
-        print('---Table Created---')
-        print(hname)
-        print(market)
-        print(time_frame)
-        print('-------')
-
-    else: # Table exist
-        return
+        #
